0eee2e59f18d173270cd77120dffff64bd2a3d81
yamhochi <yam.hoechee@gmail.com>
2023-12-01 21:31:09 +1100

    change theme

--- src/site.config.ts
+++ src/site.config.ts
@@ -6,7 +6,7 @@ export const siteConfig: SiteConfig = {
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Andie Yam",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "Andie Yam Product Designer",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
