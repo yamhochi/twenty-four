1fd07bd5312951ddc4c148202e5a656e0e0fda7c
yamhochi <yam.hoechee@gmail.com>
2024-04-07 09:59:09 +1000

    update astro icon

--- astro.config.ts
+++ astro.config.ts
@@ -7,50 +7,50 @@ import prefetch from "@astrojs/prefetch";
 import remarkUnwrapImages from "remark-unwrap-images";
 import { remarkReadingTime } from "./src/utils/remark-reading-time";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://andieyam.com/",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		remarkRehype: { footnoteLabelProperties: { className: [""] } },
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		prefetch(),
-	],
-	image: {
-		domains: ["webmention.io"],
-	},
-	vite: {
-		plugins: [rawFonts([".ttf"])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  // ! Please remember to replace the following site property with your own domain
+  site: "https://andieyam.com/",
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""]
+      }
+    },
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true
+    }
+  },
+  integrations: [mdx({}), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), prefetch(), icon()],
+  image: {
+    domains: ["webmention.io"]
+  },
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
-
 function rawFonts(ext: Array<string>) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some(e => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null
+        };
+      }
+    }
+  };
 }
